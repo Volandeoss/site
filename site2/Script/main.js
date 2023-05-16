1f3d300c5dd04b114ad36df2c4482756899c7d0d
Volandeoss <115716514+Volandeoss@users.noreply.github.com>
2023-05-16 21:42:59 +0300

    Add files via upload

---
+++ site2/Script/main.js
@@ -0,0 +1,63 @@
+
+const tab_Headers = document.querySelectorAll('[data-tab]');
+
+const contentBoxes = document.querySelectorAll('[data-tab-content]');
+
+tab_Headers.forEach(function (item) {
+    item.addEventListener('click', function () {
+        contentBoxes.forEach(function (item) {
+            item.classList.add('hidden');
+        });
+
+
+        const contentBox = document.querySelector('#' + this.dataset.tab);
+        contentBox.classList.remove('hidden');
+
+    })
+})
+
+const button = document.getElementById('my-button');
+const targetSection = document.getElementById('target-section');
+
+button.addEventListener('click', function() {
+  targetSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+
+const form = document.querySelector('.subcribe__form');
+const emailInput = form.querySelector('.subcribe__input');
+
+
+function isValidEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+
+function showError(message) {
+  const errorElem = document.createElement('div');
+  errorElem.className = 'subcribe__error';
+  errorElem.innerHTML = message;
+  form.insertBefore(errorElem, emailInput);
+}
+
+// Функція для відправки форми
+function submitForm(event) {
+
+  event.preventDefault();
+
+
+  const email = emailInput.value.trim();
+
+
+  if (!isValidEmail(email)) {
+
+    showError('Не правильний e-mail');
+  } else {
+
+    form.submit();
+  }
+}
+
+// обробник події відправки форми
+form.addEventListener('submit', submitForm);
